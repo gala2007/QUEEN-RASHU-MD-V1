a380f393555786c834013a87fd3a518205532aa5
gala2007 <pmaduswara@gmail.com>
2025-02-06 12:30:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝐐𝐔𝐄𝐄𝐍-𝐑𝐀𝐒𝐇𝐔-𝐌𝐃=MgIUUKrS#svk0RMW3BX_6Pr26W8-XzgV4Z5NrReHks9DCkBdPL1w",
 //උඩ ☝️ තියෙන Past Sesion Id කියන එක අයින් කරලා ඔයාගෙ Sesion Id එක Past කරන්න 😁
-MONGODB: process.env.MONGODB || "Past Your MongoDB Url 🍃",
+MONGODB: process.env.MONGODB || "mongodb://mongo:rSlozxLjOJcsfwFXPbLCbNiPTjvpXUOu@junction.proxy.rlwy.net:18690",
 //උඩ ☝️ තියෙන Past MongoDB Url කියන එක අයින් කරලා ඔයාගෙ MongoDB Url එක Past කරන්න 😁
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/BsjkCDP/9555.jpg",
 BOT_NAME: process.env.BOT_NAME || "QUEEN-RASHU-MD",
